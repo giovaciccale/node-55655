84b0692f629689c473be8709791359585e1d374a
giovaciccale <65725536+giovaciccale@users.noreply.github.com>
2023-12-17 20:01:40 -0300

    Ultimo commit (#6)

--- 04_fs/tickets.prom.js
+++ 04_fs/tickets.prom.js
@@ -8,3 +8,12 @@ const datos = JSON.stringify([{ title: "hp3"}], null, 2);
 fs.promises.writeFile(ruta,datos)
 .then((resultado) => console.log(resultado))
 .catch((error) => console.log(error));
+
+let configuracion = "utf-8";
+fs.promises.readFile(ruta,configuracion)
+.then((resultado) => console.log(JSON.parse(resultado)))
+.catch((error) => console.log(error));
+
+// fs.promises.unlink(ruta)
+// .then((resultado) => console.log(resultado))
+// .catch((error) => console.log(error));
